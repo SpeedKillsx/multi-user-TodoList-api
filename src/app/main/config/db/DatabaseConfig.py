77cd0a7ce77f7a1c73520b04f7985204391c1d40
SpeedKillsx <amayaslabchri88@gmail.com>
2025-10-19 00:26:48 +0200

    Add env variable for Database

--- src/app/main/config/db/DatabaseConfig.py
+++ src/app/main/config/db/DatabaseConfig.py
@@ -1,13 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 from sqlmodel import create_engine, SQLModel, Session
 from src.app.main.model import User, TodoList, Task
 class DatabaseConfig():
+    load_dotenv()
     def __init__(self, database_name:str, database_password:str):
         
         self.database_name = database_name
         self.database_password = database_password
-        self.database_url = f"postgresql+psycopg2://postgres:{self.database_password}@postgres:5432/{self.database_name}"
-        
+        self.env_db = self.get_db_env()
+        self.database_url = f"postgresql+psycopg2://postgres:{self.database_password}@{self.env_db}:5432/{self.database_name}"
         self.engine = create_engine(self.database_url)
+        print(f"Connecting to database with user  = {self.env_db}")
     
     def create_all_tables(self):
         print("Creating database..")
@@ -16,3 +21,7 @@ class DatabaseConfig():
     def get_session(self):
         with Session(self.engine) as session:
             return session
+        
+    def get_db_env(self):
+        env_db = os.getenv("APP_ENV", "local")
+        return "localhost" if env_db == "local" else "postgres"
